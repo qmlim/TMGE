830a927e1092af6b96921186002d6ba1f56baba6
jeffzhang <xiaohuz7@uci.edu>
2025-03-17 04:59:38 -0700

    add getValidColorId to ensure not matches find when initialized gird

--- CandyCrush/CandyCrushGrid.py
+++ CandyCrush/CandyCrushGrid.py
@@ -7,13 +7,32 @@ class CandyCrushGrid(Grid):
         self.width = 9
         self.grid = [[None for _ in range(self.width)] for _ in range(self.height)] # create a 9x9 grid
 
-    def initializeGrid(self): # put the tile objs on the grid
+    def initializeGrid(self):
         for i in range(self.height):
             for j in range(self.width):
-                tile = CandyCrushTile(None, (i, j))
+                color_id = self.getValidColorId(i, j)
+                tile = CandyCrushTile(color_id, (i, j))
                 self.setTileAt((i, j), tile)
         return self.grid
-    
+            
+    def getValidColorId(self, row, col):
+        available_colors = list(range(1, 7))
+        if col >= 2:
+            left1 = self.getTileAt((row, col-1))
+            left2 = self.getTileAt((row, col-2))
+            if left1.getColorId() == left2.getColorId():
+                if left1.getColorId() in available_colors:
+                    available_colors.remove(left1.getColorId())
+        
+        if row >= 2:
+            up1 = self.getTileAt((row-1, col))
+            up2 = self.getTileAt((row-2, col))
+            if up1.getColorId() == up2.getColorId():
+                if up1.getColorId() in available_colors:
+                    available_colors.remove(up1.getColorId())
+        
+        return random.choice(available_colors)
+        
     def setTileAt(self, position, tile):
         row, col = position
         self.grid[row][col] = tile
