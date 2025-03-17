dff550429578c03fd377c35ff79ab9bbdb3f2091
jeffzhang <xiaohuz7@uci.edu>
2025-03-17 06:55:37 -0700

    check colums

--- CandyCrush/CandyCrushRules.py
+++ CandyCrush/CandyCrushRules.py
@@ -7,26 +7,47 @@ class CandyCrushRules(RuleSystem):
 
     def checkRows(self):
         matches = [] # [[p1,p2,p3], [p5,p6,p7]]
-        for i in range(self.gameGrid.height):
-            current_match = [] # [p1,p2,p3]
+        for row in range(self.gameGrid.height):
+            current_match = [] # [p1,p2,p3] p1:(i,j)
             current_color = None
-            for j in range(self.gameGrid.width):
-                tile = self.gameGrid.getTileAt((i, j))
-                if current_color is not None and tile.getColorId() == current_color: # current mathch last tile
-                    current_match.append((i, j))
+            for col in range(self.gameGrid.width):
+                tile = self.gameGrid.getTileAt((row, col))
+                if current_color is not None and tile.getColorId() == current_color: # p1 match p2 color
+                    current_match.append((row, col))
                 else:
                     if len(current_match) >= self.min_match:
                         matches.append(current_match)
                     current_color = tile.getColorId()
-                    current_match = [(i, j)]
+                    current_match = [(row, col)]
 
-            if len(current_match) >= self.min_match: #extra check for last row
+            if len(current_match) >= self.min_match: #extra check while end
                 matches.append(current_match)
-
         return matches
     
-    
-
-    
     def checkColumns(self):
-        pass
+        matches = []
+        for col in range(self.gameGrid.width):
+            current_match = []
+            current_color = None
+            for row in range(self.gameGrid.height):
+                tile = self.gameGrid.getTileAt((row, col))
+                if current_color is not None and tile.getColorId() == current_color:
+                    current_match.append((row, col))
+                else:
+                    if len(current_match) >= self.min_match:
+                        matches.append(current_match)
+                    current_color = tile.getColorId() if tile else None
+                    current_match = [(row, col)] if tile else []
+            
+            if len(current_match) >= self.min_match:
+                matches.append(current_match)
+        return matches
+
+    def findMatches(self):
+        row_matches = self.checkRows()
+        col_matches = self.checkColumns()
+        return row_matches + col_matches
+    
+    def hasMatches(self):
+        matches = self.findMatches()
+        return len(matches) > 0
