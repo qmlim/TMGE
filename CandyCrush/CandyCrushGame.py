dc02f942bd38cde78f41cc34fd64470dccfc6ebb
jeffzhang <xiaohuz7@uci.edu>
2025-03-17 01:33:25 -0700

    place tile on grid dev3

--- CandyCrush/CandyCrushGame.py
+++ CandyCrush/CandyCrushGame.py
@@ -2,13 +2,14 @@ from Game import Game
 import tkinter as tk
 from State import State
 import random
-from CandyCrush.CandyCrushGrid import CandyCrushGrid
 from CandyCrush.CandyCrushTile import CandyCrushTile
+from CandyCrush.CandyCrushGrid import CandyCrushGrid
 class CandyCrushGame(Game):
     def __init__(self, playerList, parent, mainMenuFrame=None, showFrameFunc=None):
         super().__init__(playerList, parent, mainMenuFrame, showFrameFunc)
         self.gameGridType = CandyCrushGrid()
         self.tiles = []
+        self.tile_frames = []
         self.selected_tile = None
     
     def gameSetUp(self):
@@ -18,45 +19,48 @@ class CandyCrushGame(Game):
     
     def placeTilesOnGrid(self):
         self.tiles = []
-        print("asdasdsa")
-
-        grid_buttons = []
+        self.tile_frames = []
+        button_positions = []
+        
         for widget in self.frame.winfo_children():
             if isinstance(widget, tk.Button) and widget.cget('text') == " ":
                 grid_info = widget.grid_info()
-                row, col = grid_info['row'], grid_info['column']
-                
-                if row < self.gameGridType.height and col < self.gameGridType.width:
-                    while len(grid_buttons) <= row:
-                        grid_buttons.append([])
-                    
-                    while len(grid_buttons[row]) <= col:
-                        grid_buttons[row].append(None)
-                    
-                    grid_buttons[row][col] = widget
+                row, col = int(grid_info['row']), int(grid_info['column'])
+                button_positions.append((widget, row, col))
         
         for i in range(self.gameGridType.height):
-            tile_row = []
-            for j in range(self.gameGridType.width):
-                if i < len(grid_buttons) and j < len(grid_buttons[i]) and grid_buttons[i][j] is not None:
-
-                    tile = CandyCrushTile(None, (i, j))
-                    tile_row.append(tile)
-                    
-                    button = grid_buttons[i][j]
-                    button.config(
-                        bg=tile.getColor(),
-                        activebackground=tile.getColor(),
-                        relief=tk.RAISED,
-                        bd=4,
-                        width=5,
-                        height=2
-                    )
-                    
-                    self.gameGridType.setTileAt(i, j, tile)
+            self.tiles.append([None] * self.gameGridType.width)
+            self.tile_frames.append([None] * self.gameGridType.width)
+        
+        for widget, row, col in button_positions:
+            button_width = widget.winfo_reqwidth()
+            button_height = widget.winfo_reqheight()
+            grid_info = widget.grid_info()
             
-            if tile_row:
-                self.tiles.append(tile_row)
+            widget.destroy()
+            
+            tile = CandyCrushTile(None, (row, col))
+            self.tiles[row][col] = tile
+            
+            colored_frame = tk.Frame(
+                self.frame,
+                bg=tile.getColor(),
+                width=button_width,
+                height=button_height,
+                bd=3,
+                relief=tk.RAISED
+            )
+
+            colored_frame.grid(
+                row=row, 
+                column=col, 
+                padx=2, 
+                pady=2, 
+                sticky=grid_info.get('sticky', '')
+            )
+
+            self.tile_frames[row][col] = colored_frame
+            self.gameGridType.setTileAt(row, col, tile)
     
     def gamePlay(self):
         self.gameState = State.RUNNING
