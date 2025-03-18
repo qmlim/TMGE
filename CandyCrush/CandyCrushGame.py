90c1301c653cdbe918a74fe780e1832461228742
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-03-18 04:09:39 -0700

    multiplayer candy crush

--- CandyCrush/CandyCrushGame.py
+++ CandyCrush/CandyCrushGame.py
@@ -15,14 +15,21 @@ class CandyCrushGame(Game):
         self.selected_position = None
         self.animation_speed = 100
         self.is_animating = False
-        self.score = 0
+        self.previousScore = 0
+        self.currentPlayer = self.playerList[0]
     
     def gameSetUp(self):
+        self.score = 0
+        self.moves = 10 # Adjust this for number of moves per game
         self.frame = super().gameSetUp()
         self.createGrid()
         
+        self.current = tk.Label(self.frame, text=f"Current Player: {self.currentPlayer.getUsername()}", font=("Arial", 14))
+        self.current.grid(row=0, column=self.gameGridType.width)
         self.score_label = tk.Label(self.frame, text=f"Score: {self.score}", font=("Arial", 14))
-        self.score_label.grid(row=self.gameGridType.height + 1, columnspan=self.gameGridType.width, pady=10)
+        self.score_label.grid(row=1, column=self.gameGridType.width)
+        self.movesLeft = tk.Label(self.frame, text=f"Moves Left: {self.moves}", font=("Arial", 14))
+        self.movesLeft.grid(row=2, column=self.gameGridType.width)
         
         self.checkForMatches()
         return self.frame
@@ -50,7 +57,7 @@ class CandyCrushGame(Game):
             self.tiles.append(tile_row)
 
     def onTileClick(self, event, row, col):
-        if self.is_animating:
+        if self.is_animating or self.moves == 0:
             return
         
         clicked_position = (row, col)
@@ -118,6 +125,8 @@ class CandyCrushGame(Game):
     def checkAfterSwap(self, p1, p2):
         matches = self.ruleSystem.findMatches()
         if matches:
+            self.moves -= 1
+            self.movesLeft.config(text=f"Moves Left: {self.moves}")
             self.clearMatches(matches)
         else:
             self.frame.after(200, lambda: self.swapTilesBack(p1, p2))
@@ -240,10 +249,36 @@ class CandyCrushGame(Game):
             self.clearMatches(matches)
         else:
             self.is_animating = False
+            if self.moves == 0:
+                if self.currentPlayer != self.playerList[1]:
+                    self.previousScore = self.score
+                    next = tk.Button(self.frame, text="Next Player", command=self.swapPlayer)
+                    next.grid(row=self.gameGridType.height, columnspan=self.gameGridType.width)
+                else:
+                    if self.score > self.previousScore:
+                        result = self.playerList[1].getUsername() + " Wins!"
+                        self.playerList[1].addWin()
+                    elif self.score < self.previousScore:
+                        result = self.playerList[0].getUsername() + " Wins!"
+                        self.playerList[0].addWin()
+                    else:
+                        result = "Tie"
+                        self.playerList[0].addWin()
+                        self.playerList[1].addWin()
+                    matchResult = tk.Label(self.frame, text = f"Result: {result}")
+                    matchResult.grid(row=self.gameGridType.height, columnspan=self.gameGridType.width)
     
     def updateScoreDisplay(self):
         if hasattr(self, 'score_label'):
             self.score_label.config(text=f"Score: {self.score}")
+
+    def swapPlayer(self):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+        
+        self.currentPlayer = self.playerList[1]
+        self.gameSetUp()
+
 
     def gamePlay(self):
         pass
